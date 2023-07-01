59421b4249b5933ae0ee80bab14cb79ca1ad789b
Erick Meza <erickisrael203@gmail.com>
2023-07-01 15:58:59 -0500

    Creacion del repoen Git

---
+++ src/main/java/com/integrador/AppVetDEMA/AppVetDemaApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.AppVetDEMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVetDemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVetDemaApplication.class, args);
+	}
+
+}
